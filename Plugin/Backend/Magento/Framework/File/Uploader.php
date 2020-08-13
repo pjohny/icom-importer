758fd869015c56faff60886ce5108ae1f9c79e8d
janos <janos.pinczes@oander.hu>
2020-08-13 10:09:49 +0200

    first try

---
+++ Plugin/Backend/Magento/Framework/File/Uploader.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Icom\Importer\Plugin\Backend\Magento\Framework\File;
+
+class Uploader
+{
+
+    public function beforeSetAllowedExtensions(
+        \Magento\Framework\File\Uploader $subject,
+        $extensions = []
+    ) {
+        //Your plugin code
+        if(in_array("csv", $extensions) && in_array("zip", $extensions))
+        {
+            $extensions[] = "icom";
+        }
+        return [$extensions];
+    }
+}
